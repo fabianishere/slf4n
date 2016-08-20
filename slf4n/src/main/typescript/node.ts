5adba81248fb4c4eea86f4e563255b5f193b56b0
Fabian Mastenbroek <mail.fabianm@gmail.com>
2016-08-21 00:21:51 +0200

    slf4n: fix copyright header

--- slf4n/src/main/typescript/node.ts
+++ slf4n/src/main/typescript/node.ts
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2016 Fabian M. <mail.fabianm@gmail.com>
+ * Copyright (c) 2016 Fabian Mastenbroek <mail.fabianm@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
